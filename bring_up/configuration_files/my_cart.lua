163e6bfc562aafeeffac26f80a0872d990039fa7
adayimaxiga <adayimaxiga@hotmail.com>
2018-08-09 09:47:04 +0800

    gazebo environment test ok

---
+++ bring_up/configuration_files/my_cart.lua
@@ -0,0 +1,51 @@
+include "map_builder.lua"
+include "trajectory_builder.lua"
+
+options = {
+  map_builder = MAP_BUILDER,
+  trajectory_builder = TRAJECTORY_BUILDER,
+  map_frame = "map",
+  tracking_frame = "base_link",
+  published_frame = "base_link",
+  odom_frame = "odom",
+  provide_odom_frame = false,
+  use_odometry = true,
+  use_nav_sat = false,
+  use_landmarks = false,
+  publish_frame_projected_to_2d = false,
+  num_laser_scans = 2,
+  num_subdivisions_per_laser_scan = 10,
+  num_multi_echo_laser_scans = 0,
+  num_point_clouds = 0,
+  num_multi_echo_laser_scans = 0,
+  lookup_transform_timeout_sec = 0.2,
+  submap_publish_period_sec = 0.3,
+  pose_publish_period_sec = 5e-3,
+  trajectory_publish_period_sec = 30e-3,
+  rangefinder_sampling_ratio = 1.0,
+  fixed_frame_pose_sampling_ratio = 1.,
+  odometry_sampling_ratio = 1.,
+  imu_sampling_ratio = 1.,
+  landmarks_sampling_ratio = 1.,
+}
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+
+TRAJECTORY_BUILDER_2D.min_range = 0.05
+TRAJECTORY_BUILDER_2D.max_range = 20.0
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 1.0
+TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 60
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(3.)
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data =
+options.num_laser_scans * options.num_subdivisions_per_laser_scan
+
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
+
+POSE_GRAPH.constraint_builder.min_score = 0.82
+POSE_GRAPH.constraint_builder.sampling_ratio = 1.
+
+return options
