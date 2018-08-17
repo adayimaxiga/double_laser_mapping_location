d73964705bf700321bad3b4677d870a44711351f
adayimaxiga <adayimaxiga@hotmail.com>
2018-08-17 21:17:35 +0800

    add laser receive and tf to loop clousre check

--- loop_closer_check/src/node_main.cpp
+++ loop_closer_check/src/node_main.cpp
@@ -9,6 +9,8 @@
 #include "cartographer/mapping/2d/probability_grid.h"
 #include "cartographer/mapping/probability_values.h"
 #include "cartographer/sensor/point_cloud.h"
+#include "cartographer/sensor/timed_point_cloud_data.h"
+
 #include "cartographer/transform/rigid_transform_test_helpers.h"
 #include "gtest/gtest.h"
 #include "ros/ros.h"
@@ -28,9 +30,19 @@
 #include "sensor_msgs/LaserScan.h"
 #include "geometry_msgs/Pose.h"
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
+#include "geometry_msgs/Transform.h"
+#include "geometry_msgs/TransformStamped.h"
 
 #include <cmath>
 #include <iostream>
+
+#include "cartographer/common/math.h"
+#include "cartographer/common/port.h"
+#include "cartographer/common/time.h"
+
+#include "cartographer/transform/rigid_transform.h"
+#include "cartographer/transform/transform.h"
+
 //思路 map中进行接收地图数据，激光直接接收并处理，
 //amcl_pose中做扫描匹配，确定是不是进行branch and bound
 
@@ -43,21 +55,35 @@ namespace {
 
     static const std::string scan_topic_1 = "scan_1";
     static const std::string scan_topic_2 = "scan_2";
-
+    static const std::string tracking_frame_ = "base_link";
     class LoopClosuerCheck{
     public:
         LoopClosuerCheck()
         {
             map_sub_= nh_.subscribe("map", 1, &LoopClosuerCheck::mapReceived,this);
             amcl_pose_sub_ = nh_.subscribe("amcl_pose", 1, &LoopClosuerCheck::amclPoseReceived,this);
-            laser1_sub_ = nh_.subscribe("scan_1", 1, &LoopClosuerCheck::LaserScanReceived_1,this);
-            laser2_sub_ = nh_.subscribe("scan_2", 1, &LoopClosuerCheck::LaserScanReceived_2,this);
+            laser1_sub_ = nh_.subscribe(scan_topic_1, 1, &LoopClosuerCheck::LaserScanReceived_1,this);
+            laser2_sub_ = nh_.subscribe(scan_topic_2, 1, &LoopClosuerCheck::LaserScanReceived_2,this);
+
+            tfb_.reset(new tf2_ros::TransformBroadcaster());
+            tf_.reset(new tf2_ros::Buffer());
+            tfl_.reset(new tf2_ros::TransformListener(*tf_));
         }
         ~LoopClosuerCheck()
         {
         }
 
+        std::shared_ptr<tf2_ros::TransformBroadcaster> tfb_;
+        std::shared_ptr<tf2_ros::TransformListener> tfl_;
+        std::shared_ptr<tf2_ros::Buffer> tf_;
+
+
         void handleMapMessage(const nav_msgs::OccupancyGrid& msg);
+        void
+        HandleLaserScan(
+                const std::string& sensor_id, const cartographer::common::Time time,
+                const std::string& frame_id,
+                const cartographer::sensor::PointCloudWithIntensities& points);
 
         void printMap();
         //call_back;
@@ -66,7 +92,17 @@ namespace {
         void LaserScanReceived_2(const sensor_msgs::LaserScanConstPtr& laser_scan);
         void amclPoseReceived(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg);
 
-    protected:
+        std::tuple<::cartographer::sensor::PointCloudWithIntensities, ::cartographer::common::Time>
+        LaserScanToPointCloudWithIntensities(const sensor_msgs::LaserScan& msg);
+        ::cartographer::common::Time FromRos(const ::ros::Time& time);
+        ::ros::Time ToRos(::cartographer::common::Time time);
+
+
+        ::cartographer::transform::Rigid3d ToRigid3d(const geometry_msgs::TransformStamped& transform);
+        ::cartographer::transform::Rigid3d ToRigid3d(const geometry_msgs::Pose& pose);
+        Eigen::Vector3d ToEigen(const geometry_msgs::Vector3& vector3);
+        Eigen::Quaterniond ToEigen(const geometry_msgs::Quaternion& quaternion);
+        protected:
         ProbabilityGrid *probability_grid_;
         ros::NodeHandle nh_;
         ros::Subscriber map_sub_,amcl_pose_sub_;
@@ -79,37 +115,55 @@ namespace {
 
     };
 
-
-
-
-
-    //调试用，打印地图和机器人位置。
-    void
-    LoopClosuerCheck::printMap() {
-        std::cout<<"robot position in resolution is x:"
-        <<(int)((0-origin_pose_x)/map_save.info.resolution)
-        <<" y:"<<(int)((0-origin_pose_y)/map_save.info.resolution);
-        for(int j=0;j<map_save.info.height;j++) {
-            for (int i = 0; i < map_save.info.width; i++) {
-                //这里机器人位置是相对于地图origin 的位置，因此要减去origin_pose_x；
-                //后面把0改成要减去的机器人位置。
-                if((i==((int)((0-origin_pose_x)/map_save.info.resolution)))
-                &&(j==((int)((0-origin_pose_y)/map_save.info.resolution)))) {
-                    std::cout << "0";
-                    //输出机器人位置
-                }else
-                {
-                    if ((float) map_save.data[i + j * map_save.info.width] / 100.f > 0.65) {
-                        std::cout << "*";
-                    } else
-                        std::cout << " ";
-                }
-            }
-            std::cout<<std::endl;
-        }
-
+//***********************convert tools****************************
+    ::cartographer::transform::Rigid3d
+    LoopClosuerCheck::ToRigid3d(const geometry_msgs::TransformStamped& transform) {
+        return ::cartographer::transform::Rigid3d(ToEigen(transform.transform.translation),
+                       ToEigen(transform.transform.rotation));
     }
 
+    ::cartographer::transform::Rigid3d
+    LoopClosuerCheck::ToRigid3d(const geometry_msgs::Pose& pose) {
+        return ::cartographer::transform::Rigid3d({pose.position.x, pose.position.y, pose.position.z},
+                       ToEigen(pose.orientation));
+    }
+    Eigen::Vector3d
+    LoopClosuerCheck::ToEigen(const geometry_msgs::Vector3& vector3) {
+        return Eigen::Vector3d(vector3.x, vector3.y, vector3.z);
+    }
+
+    Eigen::Quaterniond
+    LoopClosuerCheck::ToEigen(const geometry_msgs::Quaternion& quaternion) {
+        return Eigen::Quaterniond(quaternion.w, quaternion.x, quaternion.y,
+                                  quaternion.z);
+    }
+
+
+
+
+    ::ros::Time
+    LoopClosuerCheck::ToRos(::cartographer::common::Time time) {
+        int64_t uts_timestamp = ::cartographer::common::ToUniversal(time);
+        int64_t ns_since_unix_epoch =
+                (uts_timestamp -
+                 ::cartographer::common::kUtsEpochOffsetFromUnixEpochInSeconds *
+                 10000000ll) *
+                100ll;
+        ::ros::Time ros_time;
+        ros_time.fromNSec(ns_since_unix_epoch);
+        return ros_time;
+    }
+
+    ::cartographer::common::Time
+    LoopClosuerCheck::FromRos(const ::ros::Time& time) {
+        // The epoch of the ICU Universal Time Scale is "0001-01-01 00:00:00.0 +0000",
+        // exactly 719162 days before the Unix epoch.
+        return ::cartographer::common::FromUniversal(
+                (time.sec +
+                 ::cartographer::common::kUtsEpochOffsetFromUnixEpochInSeconds) *
+                10000000ll +
+                (time.nsec + 50) / 100);  // + 50 to get the rounding correct.
+    }
 
 
 //***********************handle map****************************
@@ -140,10 +194,94 @@ namespace {
         //printMap();
 
     }
+    //调试用，打印地图和机器人位置。
+    void
+    LoopClosuerCheck::printMap() {
+        std::cout<<"robot position in resolution is x:"
+                 <<(int)((0-origin_pose_x)/map_save.info.resolution)
+                 <<" y:"<<(int)((0-origin_pose_y)/map_save.info.resolution);
+        for(int j=0;j<map_save.info.height;j++) {
+            for (int i = 0; i < map_save.info.width; i++) {
+                //这里机器人位置是相对于地图origin 的位置，因此要减去origin_pose_x；
+                //后面把0改成要减去的机器人位置。
+                if((i==((int)((0-origin_pose_x)/map_save.info.resolution)))
+                   &&(j==((int)((0-origin_pose_y)/map_save.info.resolution)))) {
+                    std::cout << "0";
+                    //输出机器人位置
+                }else
+                {
+                    if ((float) map_save.data[i + j * map_save.info.width] / 100.f > 0.65) {
+                        std::cout << "*";
+                    } else
+                        std::cout << " ";
+                }
+            }
+            std::cout<<std::endl;
+        }
+
+    }
+//***************************************************handle laser**************
+
+//转换到珊格地图下
+std::tuple<::cartographer::sensor::PointCloudWithIntensities, ::cartographer::common::Time>
+    LoopClosuerCheck::LaserScanToPointCloudWithIntensities(const sensor_msgs::LaserScan& msg) {
+        ::cartographer::sensor::PointCloudWithIntensities point_cloud;
+
+        float angle = msg.angle_min;
+        for (size_t i = 0; i < msg.ranges.size(); ++i) {
+            const auto& echoes = msg.ranges[i];
+            const float first_echo = echoes;
+            if (msg.range_min <= first_echo && first_echo <= msg.range_max)
+            {
+                const Eigen::AngleAxisf rotation(angle, Eigen::Vector3f::UnitZ());
+                Eigen::Vector4f point;
+                point << rotation * (first_echo * Eigen::Vector3f::UnitX()),
+                        i * msg.time_increment;
+                point_cloud.points.push_back(point);
+                if (msg.intensities.size() > 0) {
+                    const auto& echo_intensities = msg.intensities[i];
+                    point_cloud.intensities.push_back(echo_intensities);
+                } else {
+                    point_cloud.intensities.push_back(0.f);
+                }
+            }
+
+            angle += msg.angle_increment;
+
+        }
+        ::cartographer::common::Time timestamp = FromRos(msg.header.stamp);
+        if (!point_cloud.points.empty()) {
+            const double duration = point_cloud.points.back()[3];
+            timestamp += cartographer::common::FromSeconds(duration);
+            for (Eigen::Vector4f& point : point_cloud.points) {
+                point[3] -= duration;
+            }
+        }
+        return std::make_tuple(point_cloud, timestamp);
+    }
+    //这里做坐标系变换。
+    void
+    LoopClosuerCheck::HandleLaserScan(
+        const std::string& sensor_id, const cartographer::common::Time time,
+        const std::string& frame_id,
+        const cartographer::sensor::PointCloudWithIntensities& points
+        ) {
+
+        if (points.points.empty()) {
+            return;
+        }
+        ::ros::Time requested_time = ToRos(time);
+        //tfbuffer
+        ::cartographer::transform::Rigid3d sensor_to_tracking(ToRigid3d(tf_->lookupTransform(
+                tracking_frame_, frame_id, requested_time, ros::Duration(1.0))));
 
 
+            cartographer::sensor::TimedPointCloudData point_cloud_processed{
+                    time, sensor_to_tracking.translation().cast<float>(),
+                    cartographer::sensor::TransformTimedPointCloud(
+                            points.points, sensor_to_tracking.cast<float>())};
 
-
+    }
 //***********************callback***********************
 
     void
@@ -158,9 +296,12 @@ namespace {
 
     }
     void
-    LoopClosuerCheck::LaserScanReceived_1(const sensor_msgs::LaserScanConstPtr& laser_scan)
+    LoopClosuerCheck::LaserScanReceived_1(const sensor_msgs::LaserScanConstPtr& msg)
     {
-
+        ::cartographer::sensor::PointCloudWithIntensities point_cloud;
+        ::cartographer::common::Time time;
+        std::tie(point_cloud, time) = LaserScanToPointCloudWithIntensities(*msg);
+        HandleLaserScan("0", time, msg->header.frame_id, point_cloud);
     }
     void
     LoopClosuerCheck::LaserScanReceived_2(const sensor_msgs::LaserScanConstPtr& laser_scan)
@@ -174,7 +315,7 @@ namespace {
 }
 }
 
-
+//主函数没啥东西，构造一下就等着收货吧
 int main(int argc, char** argv) {
 
     ros::init(argc, argv, "LoopClosuerCheck");
